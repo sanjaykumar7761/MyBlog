a93c2fe1f7273f7eb7fb88fbc7076f50d73e89cb
sanjaykumar7761 <sk0429402@gmail.com>
2023-09-12 15:36:27 +0530

    1stStament

--- src/main/java/com/blog/entities/User.java
+++ src/main/java/com/blog/entities/User.java
@@ -24,6 +24,7 @@ public class User {
 
     private String harendra;
 
+    private String Sanjay;
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
